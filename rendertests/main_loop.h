95579c98d58ee478a63a59ab73a5ca9fc39d75f3
ErrorVania <44887293+ErrorVania@users.noreply.github.com>
2020-12-10 05:56:06 +0100

    time support

--- rendertests/main_loop.h
+++ rendertests/main_loop.h
@@ -9,16 +9,9 @@
 #include "extractPDUinfo.h"
 #include <imgui/imfilebrowser.h>
 
-inline bool endswith(std::string const& value, std::string const& ending)
-{
-    if (ending.size() > value.size()) return false;
-    return std::equal(ending.rbegin(), ending.rend(), value.rbegin());
-}
 
 void doStuff(GLFWwindow* window) {
-    ImGui::FileBrowser fb;
-    fb.SetTitle("Select a PCAP");
-    fb.SetTypeFilters({ ".pcap" });
+    
 
     std::vector<pcap_pak_hdr*> pdus;
     PcapReader pcapreader;
@@ -26,19 +19,18 @@ void doStuff(GLFWwindow* window) {
     const pcap_global_hdr* global_hdr = nullptr;
 
     bool showtest = false;
-    bool pcap_global_info = true;
     bool pcapLoaded = false;
     bool openPcapDialouge = true;
-    bool showfiledialouge = false;
     bool btnPressAuto = false;
-
-    bool* bools = nullptr;
-
     char buffer[MAX_PATH];
-    memset(buffer, 0, MAX_PATH);
-    //strcpy_s(buffer, "");
+    const ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+    ImGui::FileBrowser fb;
+    const std::vector<const char*> columns = {"Nr.","Time","Source","Destination","Protocol","Length"};
 
-    ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
+    memset(buffer, 0, MAX_PATH);
+    fb.SetTitle("Select a PCAP");
+    fb.SetTypeFilters({ ".pcap" });
 
     // Main loop
     while (!glfwWindowShouldClose(window))
@@ -54,24 +46,24 @@ void doStuff(GLFWwindow* window) {
 
         if (openPcapDialouge) {
            
-            ImGui::Begin("Open Pcap",&openPcapDialouge,ImGuiWindowFlags_::ImGuiWindowFlags_AlwaysAutoResize);
+            ImGui::Begin("Open Pcap",0,ImGuiWindowFlags_::ImGuiWindowFlags_AlwaysAutoResize);
             ImGui::Text("%.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
             ImGui::InputText("Select File", buffer, MAX_PATH);
             ImGui::SameLine();
             if (ImGui::Button("Open") || btnPressAuto) {
                 struct stat s;
                 stat(buffer, &s);
-                if (s.st_mode & S_IFDIR) {
-                    showfiledialouge = true;
-                    pcap_global_info = false;
-                    pcapLoaded = false;
-                }
-                else if (s.st_mode & S_IFREG) {
-                    showfiledialouge = false;
+                if (s.st_mode & S_IFREG) {
                     pcapreader.open(buffer);
                     global_hdr = pcapreader.getGHDR();
-                    pcapreader.beginRead(&pdus);
-                    pcapLoaded = true;
+                    if (global_hdr->magic == 0xA1B2C3D4) {
+                        pcapreader.beginRead(&pdus);
+                        pcapLoaded = true;
+                    }
+                    else {
+                        pcapLoaded = false;
+                        MessageBoxA(NULL, "Invalid file Format or wrong endianness!","PCAP Error",MB_OK);
+                    }
                 }
                 btnPressAuto = false;
             }
@@ -87,38 +79,23 @@ void doStuff(GLFWwindow* window) {
 
 
 
-            if (pcapLoaded) {
-                ImGui::Checkbox("Show Global Pcap Info", &pcap_global_info);
+            if (pcapLoaded && global_hdr) {
+                //ImGui::Checkbox("Show Global Pcap Info", &pcap_global_info);
+                ImGui::Text("Pcap Version: %s.%s", std::to_string(global_hdr->version_major).c_str(), std::to_string(global_hdr->version_minor).c_str());
+
+
+                {
+                    std::stringstream stream;
+                    stream << std::hex << global_hdr->magic;
+                    std::string result(stream.str());
+                    for (unsigned x = 0; x < result.size(); x++) result[x] = std::toupper(result[x]);
+                    ImGui::Text("Magic Number: 0x%s", result.c_str());
+                }
+
+                ImGui::Text("Network: %d", global_hdr->network);
+                ImGui::Text("Captures: %d", pdus.size());
                 ImGui::Checkbox("Show Pcap Info", &showtest);
             }
-            ImGui::End();
-        }
-
-
-
-
-
-
-
-
-
-        if (pcapLoaded && pcap_global_info) {
-            ImGui::Begin("Global Pcap Header",&pcap_global_info,ImGuiWindowFlags_::ImGuiWindowFlags_AlwaysAutoResize);
-            ImGui::Text("Pcap Version: %s.%s", std::to_string(global_hdr->version_major).c_str(), std::to_string(global_hdr->version_minor).c_str());
-
-            
-            {
-                std::stringstream stream;
-                stream << std::hex << global_hdr->magic;
-                std::string result(stream.str());
-                for (unsigned x = 0; x < result.size(); x++) result[x] = std::toupper(result[x]);
-                ImGui::Text("Magic Number: 0x%s", result.c_str());
-            }
-
-            ImGui::Text("Network: %d", global_hdr->network);
-            ImGui::Text("Captures: %d", pdus.size());
-
-
             ImGui::End();
         }
 
@@ -129,20 +106,18 @@ void doStuff(GLFWwindow* window) {
 
             ImGui::SetWindowSize(ImVec2(700, 400));
             
-            if (ImGui::BeginTable("TestTable", 5,ImGuiTableFlags_::ImGuiTableFlags_Borders | ImGuiTableFlags_::ImGuiTableFlags_Resizable)) {
-                ImGui::TableSetupColumn("Nr. ");
-                ImGui::TableSetupColumn("Source");
-                ImGui::TableSetupColumn("Destination");
-		        ImGui::TableSetupColumn("Protocol");
-                ImGui::TableSetupColumn("Length");
-                
-                
+            if (ImGui::BeginTable("TestTable", columns.size(),ImGuiTableFlags_::ImGuiTableFlags_Borders | ImGuiTableFlags_::ImGuiTableFlags_Resizable)) {
+                for (const char* _a : columns) {
+                    ImGui::TableSetupColumn(_a);
+                }
                 
                 
                 ImGui::TableHeadersRow();
                 for (unsigned i = 0; i < pdus.size(); i++) {
                     ImGui::TableNextColumn();
                     ImGui::Text("%d", i);
+                    ImGui::TableNextColumn();
+                    ImGui::Text("%fs", pdus[i]->ts_usec / 1000000.0f);
                     ImGui::TableNextColumn();
                     ImGui::Text("%s", getSource(pdus[i]).c_str());
                     ImGui::TableNextColumn();
@@ -171,5 +146,4 @@ void doStuff(GLFWwindow* window) {
 
         glfwSwapBuffers(window);
     }
-    free(bools);
 }
