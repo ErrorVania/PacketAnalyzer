460d63bd39b5a1e0ed76a7af18a5ba0e973ca6c0
Joshua ullrich <adabam2003@outlook.de>
2021-01-05 13:57:35 +0100

    style

--- rendertests/main.cpp
+++ rendertests/main.cpp
@@ -1,17 +1,14 @@
-
 #include <imgui/imgui.h>
 #include <imgui/imgui_impl_glfw.h>
 #include <imgui/imgui_impl_opengl3.h>
 #include <iostream>
-
-#include <GL/gl3w.h>            // Initialize with gl3wInit()
-
+#include <GL/gl3w.h>
 #include <GLFW/glfw3.h>
-
 #include "pcapreader/pcapreader.h"
+
 #include "main_loop.h"
 
-#define VSYNC 1
+#define VSYNC 0
 
 
 static void glfw_error_callback(int error, const char* description)
@@ -21,33 +18,38 @@ static void glfw_error_callback(int error, const char* description)
 
 
 
+
+int w;
+int h;
+
+
+void resize_callback(GLFWwindow* window, int width, int height)
+{
+    w = width;
+    h = height;
+}
+
+
 int main(int, char**)
 {
     
     glfwSetErrorCallback(glfw_error_callback);
-    if (glfwInit() == GLFW_FALSE) {
-        std::cerr << "Failed GLFW Init" << std::endl;
-        exit(1);
-    }
+    if (glfwInit() == GLFW_FALSE) { std::cerr << "Failed GLFW Init" << std::endl; exit(1); }
     std::cout << "GLFW Init success" << std::endl;
 
 
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
 
-
-    GLFWwindow* window = glfwCreateWindow(1280, 720, "Dear ImGui GLFW+OpenGL3 example", NULL, NULL);
-    if (window == NULL)
-        return 1;
+    w = 1280;
+    h = 720;
+    GLFWwindow* window = glfwCreateWindow(w, h, "Dear ImGui GLFW+OpenGL3 example", NULL, NULL);
+    if (window == NULL) return 1;
     glfwMakeContextCurrent(window);
     glfwSwapInterval(VSYNC); // Enable vsync
 
 
-    if (gl3wInit() != 0)
-    {
-        std::cerr << "Failed to initialize OpenGL loader!" << std::endl;
-        exit(2);
-    }
+    if (gl3wInit() != 0) { std::cerr << "Failed to initialize OpenGL loader!" << std::endl; exit(2); }
     std::cout << "GL3W Init success" << std::endl;
 
 
@@ -60,7 +62,12 @@ int main(int, char**)
     ImGui_ImplOpenGL3_Init("#version 130");
 
 
-    doStuff(window);
+
+    ImGuiDrawer id(&w,&h);
+    glfwSetWindowSizeCallback(window, resize_callback);
+    id.doLoop(window);
+
+    //doStuff(window);
 
     ImGui_ImplOpenGL3_Shutdown();
     ImGui_ImplGlfw_Shutdown();
--- rendertests/main_loop.h
+++ rendertests/main_loop.h
@@ -10,7 +10,7 @@
 #include <imgui/imfilebrowser.h>
 
 
-void doStuff(GLFWwindow* window) {
+/*void doStuff(GLFWwindow* window) {
     
 
     std::vector<pcap_pak_hdr*> pdus;
@@ -23,6 +23,8 @@ void doStuff(GLFWwindow* window) {
     bool openPcapDialouge = true;
     bool btnPressAuto = false;
     char buffer[MAX_PATH];
+    ImVec2 fileselectorwindow = ImVec2(0,0);
+
     const ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
     ImGui::FileBrowser fb;
     const std::vector<const char*> columns = {"Nr.","Time","Source","Destination","Protocol","Length"};
@@ -45,7 +47,8 @@ void doStuff(GLFWwindow* window) {
         ImGui::ShowDemoWindow();
 
         if (openPcapDialouge) {
-           
+            ImGui::SetNextWindowPos(ImVec2(0, 0));
+
             ImGui::Begin("Open Pcap",0,ImGuiWindowFlags_::ImGuiWindowFlags_AlwaysAutoResize);
             ImGui::Text("%.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
             ImGui::InputText("Select File", buffer, MAX_PATH);
@@ -56,7 +59,7 @@ void doStuff(GLFWwindow* window) {
                 if (s.st_mode & S_IFREG) {
                     pcapreader.open(buffer);
                     global_hdr = pcapreader.getGHDR();
-                    if (global_hdr->magic == 0xA1B2C3D4) {
+                    if (global_hdr != nullptr && global_hdr->magic == 0xA1B2C3D4) {
                         pcapreader.beginRead(&pdus);
                         pcapLoaded = true;
                     }
@@ -77,12 +80,8 @@ void doStuff(GLFWwindow* window) {
                 strcpy_s(buffer, fb.GetSelected().string().c_str());
             }
 
-
-
             if (pcapLoaded && global_hdr) {
-                //ImGui::Checkbox("Show Global Pcap Info", &pcap_global_info);
                 ImGui::Text("Pcap Version: %s.%s", std::to_string(global_hdr->version_major).c_str(), std::to_string(global_hdr->version_minor).c_str());
-
 
                 {
                     std::stringstream stream;
@@ -91,22 +90,22 @@ void doStuff(GLFWwindow* window) {
                     for (unsigned x = 0; x < result.size(); x++) result[x] = std::toupper(result[x]);
                     ImGui::Text("Magic Number: 0x%s", result.c_str());
                 }
-
                 ImGui::Text("Network: %d", global_hdr->network);
                 ImGui::Text("Captures: %d", pdus.size());
                 ImGui::Checkbox("Show Pcap Info", &showtest);
             }
+            fileselectorwindow = ImGui::GetWindowSize();
             ImGui::End();
         }
 
 
 
         if (pcapLoaded && showtest) {
-            ImGui::Begin("Test", &showtest);
-
-            ImGui::SetWindowSize(ImVec2(700, 400));
+            ImGui::SetNextWindowPos(ImVec2(fileselectorwindow.x, 0));
+            ImGui::Begin("Test", &showtest, ImGuiWindowFlags_::ImGuiWindowFlags_AlwaysAutoResize);
+            //ImGui::SetWindowSize(ImVec2(700, 400));
             
-            if (ImGui::BeginTable("TestTable", columns.size(),ImGuiTableFlags_::ImGuiTableFlags_Borders | ImGuiTableFlags_::ImGuiTableFlags_Resizable)) {
+            if (ImGui::BeginTable("Data", columns.size(),ImGuiTableFlags_::ImGuiTableFlags_Borders | ImGuiTableFlags_::ImGuiTableFlags_Resizable)) {
                 for (const char* _a : columns) {
                     ImGui::TableSetupColumn(_a);
                 }
@@ -117,7 +116,7 @@ void doStuff(GLFWwindow* window) {
                     ImGui::TableNextColumn();
                     ImGui::Text("%d", i);
                     ImGui::TableNextColumn();
-                    ImGui::Text("%fs", pdus[i]->ts_usec / 1000000.0f);
+                    ImGui::Text("%.3fs", pdus[i]->ts_usec / 1000000.0f);
                     ImGui::TableNextColumn();
                     ImGui::Text("%s", getSource(pdus[i]).c_str());
                     ImGui::TableNextColumn();
@@ -146,4 +145,161 @@ void doStuff(GLFWwindow* window) {
 
         glfwSwapBuffers(window);
     }
-}
+}*/
+
+
+
+class ImGuiDrawer {
+public:
+
+    std::vector<pcap_pak_hdr*> pdus;
+    PcapReader pcapreader;
+
+    const pcap_global_hdr* global_hdr;
+    bool showtest, pcapLoaded, openPcapDialouge, btnPressAuto;
+    char buffer[MAX_PATH];
+    ImVec2 fileselectorwindowsize;
+    ImVec4 clear_color;
+    ImGui::FileBrowser fb;
+    std::vector<const char*> columns;
+    const int* w;
+    const int* h;
+
+    ImGuiDrawer(const int* width, const int* height) {
+        w = width;
+        h = height;
+        global_hdr = nullptr;
+
+        showtest = false;
+        pcapLoaded = false;
+        openPcapDialouge = true;
+        btnPressAuto = false;
+        fileselectorwindowsize = ImVec2(0, 0);
+
+        clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+        columns = { "Nr.","Time","Source","Destination","Protocol","Length" };
+
+
+        memset(buffer, 0, MAX_PATH);
+        fb.SetTitle("Select a PCAP");
+        fb.SetTypeFilters({ ".pcap" });
+    }
+
+
+    void doLoop(GLFWwindow* window) {
+        while (!glfwWindowShouldClose(window))
+        {
+            glfwPollEvents();
+
+            ImGui_ImplOpenGL3_NewFrame();
+            ImGui_ImplGlfw_NewFrame();
+            ImGui::NewFrame();
+
+
+            ImGui::ShowDemoWindow();
+
+            if (openPcapDialouge) {
+                ImGui::SetNextWindowPos(ImVec2(0, 0));
+
+                ImGui::Begin("Open Pcap", 0, ImGuiWindowFlags_::ImGuiWindowFlags_AlwaysAutoResize);
+                ImGui::Text("%.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+                ImGui::InputText("Select File", buffer, MAX_PATH);
+                ImGui::SameLine();
+                if (ImGui::Button("Open") || btnPressAuto) {
+                    struct stat s;
+                    stat(buffer, &s);
+                    if (s.st_mode & S_IFREG) {
+                        pcapreader.open(buffer);
+                        global_hdr = pcapreader.getGHDR();
+                        if (global_hdr != nullptr && global_hdr->magic == 0xA1B2C3D4) {
+                            pcapreader.beginRead(&pdus);
+                            pcapLoaded = true;
+                        }
+                        else {
+                            pcapLoaded = false;
+                            MessageBoxA(NULL, "Invalid file Format or wrong endianness!", "PCAP Error", MB_OK);
+                        }
+                    }
+                    btnPressAuto = false;
+                }
+                ImGui::SameLine();
+                if (ImGui::Button("...")) {
+                    fb.Open();
+                }
+
+                fb.Display();
+                if (fb.HasSelected()) {
+                    strcpy_s(buffer, fb.GetSelected().string().c_str());
+                }
+
+                if (pcapLoaded && global_hdr) {
+                    ImGui::Text("Pcap Version: %s.%s", std::to_string(global_hdr->version_major).c_str(), std::to_string(global_hdr->version_minor).c_str());
+
+                    {
+                        std::stringstream stream;
+                        stream << std::hex << global_hdr->magic;
+                        std::string result(stream.str());
+                        for (unsigned x = 0; x < result.size(); x++) result[x] = std::toupper(result[x]);
+                        ImGui::Text("Magic Number: 0x%s", result.c_str());
+                    }
+                    ImGui::Text("Network: %d", global_hdr->network);
+                    ImGui::Text("Captures: %d", pdus.size());
+                    ImGui::Checkbox("Show Pcap Info", &showtest);
+                }
+                fileselectorwindowsize = ImGui::GetWindowSize();
+                ImGui::End();
+            }
+
+            static Resolver res;
+
+            if (pcapLoaded && showtest) {
+                ImGui::SetNextWindowPos(ImVec2(fileselectorwindowsize.x, 0));
+                ImGui::SetNextWindowSize(ImVec2(*w - fileselectorwindowsize.x, *h));
+                ImGui::Begin("Test", &showtest);
+
+
+                if (ImGui::BeginTable("Data", columns.size(), ImGuiTableFlags_::ImGuiTableFlags_Borders | ImGuiTableFlags_::ImGuiTableFlags_Resizable)) {
+                    for (const char* _a : columns) {
+                        ImGui::TableSetupColumn(_a);
+                    }
+
+
+                    ImGui::TableHeadersRow();
+                    for (unsigned i = 0; i < pdus.size(); i++) {
+                        ImGui::TableNextColumn();
+                        ImGui::Text("%d", i);
+                        ImGui::TableNextColumn();
+                        ImGui::Text("%.3fs", pdus[i]->ts_usec / 1000000.0f);
+                        ImGui::TableNextColumn();
+                        ImGui::Text("%s", getSource(pdus[i]).c_str());
+                        ImGui::TableNextColumn();
+                        ImGui::Text("%s", getDest(pdus[i]).c_str());
+                        ImGui::TableNextColumn();
+                        ImGui::Text("%s", lastProtoL2(pdus[i]).c_str());
+                        ImGui::TableNextColumn();
+                        ImGui::Text("%d", pdus[i]->incl_len);
+                        ImGui::TableNextRow();
+                    }
+                    ImGui::EndTable();
+                }
+                ImGui::End();
+            }
+
+
+
+            // Rendering
+            ImGui::Render();
+            int display_w, display_h;
+            glfwGetFramebufferSize(window, &display_w, &display_h);
+            glViewport(0, 0, display_w, display_h);
+            glClearColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
+            glClear(GL_COLOR_BUFFER_BIT);
+            ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+            glfwSwapBuffers(window);
+        }
+    }
+
+
+
+};
